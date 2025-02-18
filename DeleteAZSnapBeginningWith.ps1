6ad9168c50afadd6f95cf989a34b332045fd278b
hdfanatic99 <77355134+hdfanatic99@users.noreply.github.com>
2025-02-18 17:44:27 -0600

    Create DeleteAZSnapBeginningWith.ps1
    Deletes all snapshots in an Azure subscription beginning with specific text/name.

---
+++ DeleteAZSnapBeginningWith.ps1
@@ -0,0 +1,31 @@
+$subscriptionName = "Azure subscription 1"
+
+# Set the subscription context
+Set-AzContext -SubscriptionName $subscriptionName
+
+# Get all snapshots in the subscription
+$snapshots = Get-AzSnapshot -ResourceGroupName * | Where-Object { $_.Name.StartsWith("Veeam") }
+
+# Start jobs to delete snapshots concurrently
+$jobs = @()
+foreach ($snapshot in $snapshots) {
+    $jobs += Start-Job -ScriptBlock {
+        param ($rgName, $snapName)
+
+        function Remove-Snapshot {
+            param (
+                [string]$ResourceGroupName,
+                [string]$SnapshotName
+            )
+            Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $SnapshotName -Force
+            Write-Output "Snapshot $SnapshotName deleted successfully."
+        }
+
+        Remove-Snapshot -ResourceGroupName $rgName -SnapshotName $snapName
+    } -ArgumentList $snapshot.ResourceGroupName, $snapshot.Name
+}
+
+# Wait for all jobs to complete
+$jobs | ForEach-Object { $_ | Wait-Job | Receive-Job }
+
+Write-Output "All snapshots have been deleted successfully."
